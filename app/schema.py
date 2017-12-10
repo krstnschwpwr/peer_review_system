b8c32a56081ba475055e21f4e9084081aae20f3a
Kristine <kristine@7mind.de>
2017-12-10 22:33:00 +0100

    added schema

--- app/schema.py
+++ app/schema.py
@@ -6,7 +6,12 @@ class UserSchema(ma.ModelSchema):
     class Meta:
         model = User
 
-
+class PaperSchema(ma.ModelSchema):
+    class Meta:
+        model = Paper
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+
+paper_schema = PaperSchema()
+papers_schema = PaperSchema(many=True)
