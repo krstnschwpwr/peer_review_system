886595252bf5a26d050de5ac893d0119c70f262a
Kristine <kristine@7mind.de>
2017-12-10 21:40:57 +0100

    init update

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, g
 from flask.ext.bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask_login import LoginManager
-from flask_openid import OpenID
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_marshmallow import Marshmallow
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,20 +13,17 @@ app.config['SECRET_KEY'] = 'super secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 db = SQLAlchemy(app)
-
+ma = Marshmallow(app)
 
 
 lm = LoginManager()
 lm.init_app(app)
-#oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app.config.from_object(__name__)
 from app import views
 from app.models import User
 
-lm.login_view = "users.login"
+#lm.login_view = "users.login"
 
 
-@lm.user_loader
-def load_user(user_id):
-    return User.query.filter(User.id == int(user_id)).first()
+
