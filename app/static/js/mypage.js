dea32a4f2d3d48db89009f0bf86c832ec1361535
Kristine <kristine@7mind.de>
2017-12-17 11:13:43 +0100

    reformatting

--- app/static/js/mypage.js
+++ app/static/js/mypage.js
@@ -1,38 +1,33 @@
-    var bla = [];
+var bla = [];
 
 $(document).ready(function () {
-  loadPapers();
-    
+    loadPapers();
 
-function loadPapers(c) {
-	if (c) {
-		$.ajax({
-			url: '/api/papers',
-			method: 'get',
-			dataType: 'json',
-            success: function(data) {
-                useReturnData(data);
-                console.log(data);
+
+    function loadPapers(c) {
+        if (c) {
+            $.ajax({
+                url: '/api/papers',
+                method: 'get',
+                dataType: 'json',
+                success: function (data) {
+                    useReturnData(data);
+                    console.log(data);
+                }
+            })
+
+        }
     }
-		})
-
-	}
-}
 
 
+    function useReturnData(data) {
+        myvar = data.papers;
 
-function useReturnData(data){
-    myvar = data.papers;
+        $.each(myvar, function (key, paper) {
+            $('.list').append('<div>' + "<h4>" +
+                paper.title + "</h4><br>" + paper.abstract + '</div><br>');
+        });
+    };
 
-     $.each(myvar, function(key, paper) {
-         $('.list').append('<div>' + "<h4>" +
-             paper.title + "</h4><br>" + paper.abstract +'</div><br>'); 
-        console.log("My name is "+paper.title+" and I am a "+paper.abstract);
-    });
-};
-
-function displayContent(d){
-    $('.mypapers li').text(d.title);
-}
 
 });
