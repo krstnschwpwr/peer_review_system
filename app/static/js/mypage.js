3b8847a2ad3eaf474b0da21a4b35094d3192e8c7
Kristine <kristine@7mind.de>
2017-12-17 11:19:12 +0100

    update

--- app/static/js/mypage.js
+++ app/static/js/mypage.js
@@ -4,30 +4,28 @@ $(document).ready(function () {
     loadPapers();
 
 
-    function loadPapers(c) {
-        if (c) {
-            $.ajax({
-                url: '/api/papers',
-                method: 'get',
-                dataType: 'json',
-                success: function (data) {
-                    useReturnData(data);
-                    console.log(data);
-                }
-            })
-
-        }
-    }
-
-
     function useReturnData(data) {
         myvar = data.papers;
 
         $.each(myvar, function (key, paper) {
-            $('.list').append('<div>' + "<h4>" +
+            $('.mypapers .list').append('<div>' + "<h4>" +
                 paper.title + "</h4><br>" + paper.abstract + '</div><br>');
         });
     };
 
+
+    function loadPapers() {
+        $.ajax({
+            url: '/api/papers',
+            method: 'get',
+            dataType: 'json',
+            success: function (data) {
+                useReturnData(data);
+                console.log(data);
+            }
+        })
+
+
+    }
 
 });
