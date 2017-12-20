ab57bcc38c5880c5ec4eb7be341a398c02ea251f
Kristine <kristine@7mind.de>
2017-12-20 23:11:45 +0100

    nav and some css

--- app/static/js/mypage.js
+++ app/static/js/mypage.js
@@ -10,13 +10,15 @@ $(document).ready(function () {
         var id = '';
         $.each(myvar, function (key, paper) {
             id = paper.id;
-            $('.mypapers .list ').append(" <li class='cf '> <div class='content'>" + paper.id + "<h4>" +
-                paper.title + "<span class='status'>" + paper.status+ "</span>" +"</h4><br>" + paper.abstract + "<br>" + "<br>" +
-                "<a href='api/paper/"+id+"'> <button class='btn orange'>Show</button></a>"+
-                "<a href='api/paper/delete/"+id+"'> <button class='btn orange'>Delete</button></a>"+
-                "</a> "  + "</form><br>" +"</a>" + "</div></li>");
+            $('.mypapers .columns ').append(" <div class='column'><div class='box'> <article class='media'> "+
+                "<div class='media-content clearfix'> <div class='content'> <small>" + paper.status + "</small>"+
+                                "<h4 class='subtitle is-5'>" + paper.title+ " </h4><br>" + paper.abstract + "</p> </div>"+
+                            "<div class='column is-narrow'></div> <a  class='button button is-link is-rounded' href='api/paper/"+id+"'>Show</a>"+
+                         "<a class='button is-link is-outlined is-rounded' href='api/paper/delete/"+id+"'> Delete</a>"+"</div> <div class='column'></div> </article> </div></div>");
         });
     }
+
+
 
     function loadPapers() {
         $.ajax({
