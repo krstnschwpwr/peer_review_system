bf6bf9ff713e3f5fa2f6b01b79492dd67aec65de
Kristine <kristine@7mind.de>
2017-12-21 12:32:13 +0100

    papers to review fix

--- app/views.py
+++ app/views.py
@@ -85,25 +85,17 @@ def logout():
     return home()
 
 
-#
-# #CRUD
-# @app.route('/paper/new')
-# #@auth.login_required
-# def new_page():
-#     return render_template('newpaper.html')
-
-
-@app.route('/mypage')
+@app.route('/mypage', methods=['GET'])
 @login_required
 def index():
-    if 'username' in session:
-        username = session['username']
-
     myreviews = Reviewer.query.filter(Reviewer.reviewer_id == current_user.id).first()
     mypapers = Paper.query.filter(Paper.id == myreviews.paper_id).all()
+    if 'username' in session:
+        username = session['username']
     return render_template('mypage.html', mypapers=mypapers)
 
 
+# User
 @app.route('/api/users', methods=['GET'])
 @login_required
 def get_users():
@@ -120,18 +112,30 @@ def get_user(id):
     return jsonify(user_schema.dump(user).data)
 
 
+# Rating
 @app.route('/mypage/rating/save', methods=['GET', 'POST'])
 def save_rating():
     if request.method == 'POST':
         rating = request.form.get('rating')
         paper_id = request.form.get('paper_id')
-        bl = Reviewer.query.filter_by(reviewer_id=current_user.id, paper_id = paper_id).first()
+        bl = Reviewer.query.filter_by(reviewer_id=current_user.id, paper_id=paper_id).first()
         rat = bl.rating
         bl.rating = rating
         db.session.commit()
     return render_template('admin.html')
 
 
+# Submission
+@app.route('/submissions', methods=['GET', 'POST'])
+def show_submissions():
+    papers = []
+    all_reviews = Reviewer.query.filter(Reviewer.rating != None).all()
+    for a in all_reviews:
+        papers.append(Paper.query.filter_by(id=a.paper_id).first())
+    return render_template('submission.html', fetch=papers)
+
+
+# Reviews
 @app.route('/review/new', methods=['GET', 'POST'])
 @login_required
 def save_review():
@@ -142,7 +146,7 @@ def save_review():
 
     if request.method == 'POST':
         paper_id = request.form.get('paper_id')
-        review = Reviewer(paper_id=paper_id, reviewer_id=request.form.get('dropdown'))
+        review = Reviewer(paper_id=paper_id, reviewer_id=request.form.get('dropdown'), rating=10)
         db.session.add(review)
         db.session.commit()
     # return (str(user_id))  #
@@ -150,16 +154,13 @@ def save_review():
     return render_template('admin.html', form=form, users=users)
 
 
-@app.route('/submissions', methods=['GET', 'POST'])
-def show_submissions():
-    papers = []
-    all_reviews = Reviewer.query.filter(Reviewer.rating != None).all()
-    for a in all_reviews:
-        papers.append(Paper.query.filter_by(id=a.paper_id).first())
-    return render_template('submission.html', fetch=papers)
+@app.route('/api/reviews', methods=['GET'])
+def get_reviews():
+    reviewers = Reviewer.query.all()
+    return jsonify(reviewers=reviewers_schema.dump(reviewers).data)
 
 
-##Papers
+# Papers
 
 @app.route('/paper/new', methods=['GET', 'POST'])
 @login_required
@@ -171,7 +172,7 @@ def save_page():
             # Get validated data from form
             title = paper_form.title.data
             abstract = paper_form.abstract.data
-            status = paper_form.status.data
+            status = request.form.get('status')
             paper = Paper(title, abstract, status="Under Review")
             db.session.add(paper)
             db.session.commit()
@@ -179,16 +180,6 @@ def save_page():
         return redirect('mypage')
 
     return render_template('newpaper.html', form=paper_form)
-
-
-def make_public_paper(paper):
-    new_paper = {}
-    for field in paper:
-        if field == 'id':
-            new_paper['uri'] = url_for('get_papers', paper_id=paper['id'], _external=True)
-        else:
-            new_paper[field] = paper[field]
-    return new_paper
 
 
 @app.route('/paper/<int:paper_id>/update', methods=['POST'])
@@ -212,15 +203,6 @@ def get_papers():
     return jsonify(papers=papers_schema.dump(papers).data)
 
 
-# @app.route('/api/reviews', methods=['GET', 'POST'])
-# def save_review():
-
-@app.route('/api/reviews', methods=['GET'])
-def get_reviews():
-    reviewers = Reviewer.query.all()
-    return jsonify(reviewers=reviewers_schema.dump(reviewers).data)
-
-
 @app.route('/api/paper/<int:paper_id>', methods=['GET'])
 @login_required
 def get_paper(paper_id):
@@ -231,6 +213,7 @@ def get_paper(paper_id):
 
 
 @app.route('/api/paper/delete/<int:paper_id>', methods=['GET', 'POST'])
+@login_required
 def delete_paper(paper_id):
     paper = Paper.query.get(paper_id)
     if not paper:
@@ -238,6 +221,16 @@ def delete_paper(paper_id):
     db.session.delete(paper)
     db.session.commit()
     return redirect('mypage')
+
+
+def make_public_paper(paper):
+    new_paper = {}
+    for field in paper:
+        if field == 'id':
+            new_paper['uri'] = url_for('get_papers', paper_id=paper['id'], _external=True)
+        else:
+            new_paper[field] = paper[field]
+    return new_paper
 
 
 if __name__ == '__main__':
